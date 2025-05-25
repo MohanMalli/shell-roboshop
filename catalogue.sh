8a827eaff67c229f663978663dba8ce9ba121fc8
MohanMalli <mmohan0140@gmail.com>
2025-05-25 08:34:36 +0530

    Roboshop Project

--- catalogue.sh
+++ catalogue.sh
@@ -16,6 +16,48 @@ echo "Script started executing at: $(date)"
 if [ $USERID -ne 0 ]
 then
     echo -e " $R ERROR :: Please run the script with root Access $N "
+    exit 1
 else
     echo -e " $G Your running with root Access $N "
 fi
+
+VALIDATE(){
+    if [ $1 -eq 0 ]
+    then
+        echo -e  " $2 is... $G SUCCESS $N "
+    else
+        echo -e  " $2 is... $R FAILURE $N "
+        exit 1    
+}
+
+dnf module disable nodejs -y
+VALIDATE $? "Disabling default nodejs"
+
+dnf module enable nodejs:20 -y
+VALIDATE $? "Enabling nodejs:20"
+
+dnf install nodejs -y
+VALIDATE $? "Installing nodejs:20"
+
+id roboshop
+if [ $? -ne 0 ]
+then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+    VALIDATE $? "Creating roboshop system user"
+else
+      echo -e "System user roboshop already created ... $Y SKIPPING $N"
+fi    
+
+mkdir -p /app
+VALIDATE $? "Creating app directory"
+
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue-v3.zip
+VALIDATE $? "Downloading Catalogue"
+
+rm -rf /app/*
+cd /app
+unzip /tmp/catalogue.zip
+VALIDATE $? "unzipping catalogue"
+
+npm install
+VALIDATE $? "Installing Dependencies"

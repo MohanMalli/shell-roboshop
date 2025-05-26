44e8fb73f1f4f136a15719c0e0672ea9bd043aa1
MohanMalli <mmohan0140@gmail.com>
2025-05-26 12:39:51 +0530

    Roboshop Project

---
+++ mysql.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+USERID=$(usr -id)
+R="\e[31m"
+Y="\e[32m"
+G="\e[33m"
+N="\e[0N"
+
+LOGS_FOLDER="/var/log/roboshop-logs"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME
+
+mkdir -p $LOGS_FOLDER
+echo " Script started Executing at: $(date) "
+
+if [ USERID -ne 0 ]
+then
+    echo -e " $R ERROR :: Please run this with root Access $N "
+else
+    echo -e " $G your running with root Access $N "
+fi
+    

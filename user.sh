e34e2c38cc974760dc2f354f03a242111e6c4773
MohanMalli <mmohan0140@gmail.com>
2025-05-26 19:30:20 +0530

    Roboshop Project

--- user.sh
+++ user.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
  
-#START_TIME=$(date +%s)
+START_TIME=$(date +%s)
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
@@ -31,5 +31,44 @@ VALIDATE(){
         echo -e " $2 is ... $R FAILURE $N "
         exit 1
     fi        
-
 }
+
+dnf module disable nodejs -y
+VALIDATE $? " Disabling nodejs "
+
+dnf module enable nodejs:20 -y
+VALIDATE $? " Enabling nodejs "
+
+dnf install nodejs -y
+VALIDATE $? " Installing nodejs "
+
+id roboshop
+if [ $? -ne 0 ]
+then
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+    VALIDATE $? "Creating roboshop system user"
+else
+      echo -e "System user roboshop already created ... $Y SKIPPING $N"
+fi  
+
+mkdir -p /app
+VALIDATE $? "Creating app directory"
+
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip 
+VALIDATE $? "Downloading user"
+
+rm -rf /app/*
+cd /app
+unzip /tmp/user.zip 
+VALIDATE $? "unzipping user"
+
+npm install 
+VALIDATE $? "Installing Dependencies"
+
+cp $SCRIPT_DIR/user.service /etc/systemd/system/user.service
+VALIDATE $? "Copying catalogue service"
+
+systemctl daemon-reload 
+systemctl enable user 
+systemctl start user 
+VALIDATE $? "user"

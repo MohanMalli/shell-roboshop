6a56f00fe1205ecaa248a8a518c4c0a1f3e516b6
MohanMalli <mmohan0140@gmail.com>
2025-05-27 17:05:46 +0530

    Roboshop Project

--- shipping.sh
+++ shipping.sh
@@ -11,6 +11,9 @@ LOGS_FOLDER="/var/log/roboshop-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
+mkdir -p $LOGS_FOLDER
+echo "Script started executing at: $(date)" 
+
 if [ $USERID -ne 0 ]
 then
     echo " $R ERROR:: Please run the script with root Access $N "

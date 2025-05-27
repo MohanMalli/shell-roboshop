00c388dbe29531225970e3a05bee164b01ed9552
MohanMalli <mmohan0140@gmail.com>
2025-05-27 22:53:07 +0530

    Roboshop Project

--- mysql.sh
+++ mysql.sh
@@ -39,13 +39,13 @@ VALIDATE(){
 }
   
   dnf install mysql-server -y &>>$LOG_FILE
-  VALIDATE $? " Installing MySQL server "
+  VALIDATE $? "Installing MySQL server"
 
   systemctl enable mysqld &>>$LOG_FILE
-  VALIDATE $? " Enabling MySQL "
+  VALIDATE $? "Enabling MySQL"
 
   systemctl start mysqld &>>$LOG_FILE
-  VALIDATE $? " Starting MySQL "
+  VALIDATE $? "Starting MySQL"
 
   mysql_secure_installation --set-root-pass $MYSQL_ROOT_PASSWORD &>>$LOG_FILE
   VALIDATE $? "Setting MySQL root password"

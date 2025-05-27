061cfea20b7d37efa0a9494fea2074a00631c989
MohanMalli <mmohan0140@gmail.com>
2025-05-27 16:26:28 +0530

    Roboshop Project

--- mysql.sh
+++ mysql.sh
@@ -21,6 +21,9 @@ else
     echo -e " $G your running with root Access $N "
 fi
 
+echo "Please enter root password to setup"
+read -s MYSQL_ROOT_PASSWORD  
+
 # validate functions takes input as exit status, what command they tried to install
 VALIDATE(){
     if [ $1 -eq 0 ]
@@ -42,6 +45,14 @@ VALIDATE(){
 
   systemctl start mysqld 
   VALIDATE $? " Starting MySQL "
+
+  mysql_secure_installation --set-root-pass $MYSQL_ROOT_PASSWORD 
+  VALIDATE $? "Setting MySQL root password"
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(( $END_TIME - $START_TIME ))
+
+echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N"
 
   
 

a404ee10cbfeea5eca93ef744443125ae025a309
MohanMalli <mmohan0140@gmail.com>
2025-05-28 14:04:49 +0530

    Roboshop Project

--- payment.sh
+++ payment.sh
@@ -1,12 +1,11 @@
-#!/bin/bash
+ #!/bin/bash
 
 START_TIME=$(date +%s)
 USERID=$(id -u)
 R="\e[31m"
 G="\e[32m"
-y="\e[33m"
+Y="\e[33m"
 N="\e[0m"
-
 LOGS_FOLDER="/var/log/roboshop-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
@@ -15,63 +14,65 @@ SCRIPT_DIR=$PWD
 mkdir -p $LOGS_FOLDER
 echo "Script started executing at: $(date)" | tee -a $LOG_FILE
 
+# check the user has root priveleges or not
 if [ $USERID -ne 0 ]
 then
-    echo -e " $R ERROR :: Please run the script with root Access $N " | tee -a $LOG_FILE
-    exit 1
+    echo -e "$R ERROR:: Please run this script with root access $N" | tee -a $LOG_FILE
+    exit 1 #give other than 0 upto 127
 else
-    echo -e " $G Your running with root Access $N " | tee -a $LOG_FILE
+    echo "You are running with root access" | tee -a $LOG_FILE
 fi
 
-VALIDATE() {
+# validate functions takes input as exit status, what command they tried to install
+VALIDATE(){
     if [ $1 -eq 0 ]
     then
-        echo -e  " $2 is... $G SUCCESS $N " | tee -a $LOG_FILE
+        echo -e "$2 is ... $G SUCCESS $N" | tee -a $LOG_FILE
     else
-        echo -e  " $2 is... $R FAILURE $N " | tee -a $LOG_FILE
+        echo -e "$2 is ... $R FAILURE $N" | tee -a $LOG_FILE
         exit 1
-    fi        
+    fi
 }
 
-dnf install python3 gcc python3-devel -y
-VALIDATE $? " Installing pytgon3 "
+dnf install python3 gcc python3-devel -y &>>$LOG_FILE
+VALIDATE $? "Install Python3 packages"
 
-id roboshop
+id roboshop &>>$LOG_FILE
 if [ $? -ne 0 ]
 then
-    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop
+    useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
     VALIDATE $? "Creating roboshop system user"
 else
-      echo -e "System user roboshop already created ... $Y SKIPPING $N"
-fi    
+    echo -e "System user roboshop already created ... $Y SKIPPING $N"
+fi
 
-mkdir -p /app
+mkdir -p /app 
 VALIDATE $? "Creating app directory"
 
-curl -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip 
+curl -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip &>>$LOG_FILE
 VALIDATE $? "Downloading payment"
 
 rm -rf /app/*
-cd /app
-unzip /tmp/payment.zip 
+cd /app 
+unzip /tmp/payment.zip &>>$LOG_FILE
 VALIDATE $? "unzipping payment"
 
-pip3 install -r requirements.txt
-VALIDATE $? "Installing Dependencies"
+pip3 install -r requirements.txt &>>$LOG_FILE
+VALIDATE $? "Installing dependencies"
 
-cp $SCRIPT_DIR/payment.service /etc/systemd/system/payment.service
-VALIDATE $? " creating a .service file "
+cp $SCRIPT_DIR/payment.service /etc/systemd/system/payment.service &>>$LOG_FILE
+VALIDATE $? "Copying payment service"
 
-systemctl daemon-reload
-VALIDATE $? " system reloading "
+systemctl daemon-reload &>>$LOG_FILE
+VALIDATE $? "Daemon Reload"
 
-systemctl enable payment 
-VALIDATE $? " Enabling payment "
+systemctl enable payment &>>$LOG_FILE
+VALIDATE $? "Enable payment"
 
-systemctl start payment
-VALIDATE $? " Starting payment "
+systemctl start payment &>>$LOG_FILE
+VALIDATE $? "Starting payment"
 
 END_TIME=$(date +%s)
 TOTAL_TIME=$(( $END_TIME - $START_TIME ))
 
-echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N"
+echo -e "Script exection completed successfully, $Y time taken: $TOTAL_TIME seconds $N" | tee -a $LOG_FILE
